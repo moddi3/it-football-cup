ccdc4a2a8a73d24a2c16d9e3bba5af6d23c07b72
Vlad Ivanov <vlad211099@gmail.com>
2018-11-25 15:39:17 +0200

    fix style

--- src/App.js
+++ src/App.js
@@ -4,23 +4,16 @@ import './App.css';
 
 class App extends Component {
   render() {
-    const errorStyle = {
-      color: 'tomato'
-    };
+    const APP_ID = process.env.REACT_APP_FACEBOOK_APP_ID;
+    const errorStyle = !APP_ID ? { fontStyle: 'italic', color: 'tomato' } : {};
 
-    const APP_ID = process.env.REACT_APP_FACEBOOK_APP_ID || (
-      <samp style={errorStyle}>
-        <i>please check your environments variables</i>
-      </samp>
-    );
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Your APP_ID - <code style={errorStyle}>{APP_ID || 'please check your environments variables'}</code>
           </p>
-          Your APP_ID - {APP_ID}
           <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
